2d715acf5397d3c63dc57edb69bbe7419a335e53
Mike Frawley <frawl021@gmail.com>
2009-08-14 14:14:04 -0500

    move serialize/desrialize into drumz.  (almost) no global state now

--- html5drums.js
+++ html5drums.js
@@ -8,13 +8,17 @@
 $(document).ready(function(){
 	make_playlist();
 	bind_buttons();
+	parse_location_hash();
 	init_state();
 	return;
 	/////
+	function parse_location_hash() {
+		if (location.hash.length > 0)
+			$.drumz.deserialize(location.hash.substring(1));
+	}
 	function make_playlist() {
 		// Process each of the audio items, creating a playlist sort of setup
 		$("audio").each(function(i){
-			// Make a self reference for ease of use in click events
 			var self = this;
 
 			// Make a sub-list for our control
@@ -37,48 +41,25 @@ $(document).ready(function(){
 	}
 	function bind_buttons() {
 		$("#soundstart").toggle($.drumz.start, $.drumz.stop);
-		$('#clearall').click(clearAll);
-		$('#reload').click(parseHash);
+		$('#clearall').click($.drumz.notes.clear);
+		$('#reload').click(parse_location_hash);
 	}
-	function init_state () {
-		parseHash();
+	function init_state () {		
 		$('#tempovalue').html($.drumz.tempo);
-
 		$('#temposlider').slider({
+			'min': 30, 'max': 180, 'step': 10, 
 			'value': $.drumz.tempo,
-			'min': 30,
-			'max': 180,
-			'step': 10,
 			'slide': function(e, ui) {
 				$('#tempovalue').html(ui.value);
 				$.drumz.change({tempo: ui.value});
 			},
-			'stop': function(e, ui) {
-				buildHash();
-			}
+			'stop': buildHash
 		});
 	}
 });
 
+// extend jQuery with drumz plugin
 (function ($) {
-	var tracker = {
-		pips: $("#tracker .pip"),
-
-		// deactivates the current pip and activates the next pip
-		// starts at pip 0 and wraps around to pip 0
-		// returns the index of the newly activated pip
-		activate_next: function () {
-			var pips = this.pips;
-			return pips.index(
-				$(pips.active().deactivate().next()[0] || pips[0]).activate()
-			);
-		}
-	};
-	
-	var sounds = {
-		
-	};
-	// extend jQuery
 	$.extend({drumz: {
 		defaults: {
 			playing: false,
@@ -87,7 +68,7 @@ $(document).ready(function(){
 		},
 		start: function () {
 			$.drumz.playing = true;
-			$.drumz.loop = setInterval(playBeat, 60000 / $.drumz.tempo / 4);
+			$.drumz.loop = setInterval($.drumz.play, 60000 / $.drumz.tempo / 4);
 		},
 		stop: function () {
 			$.drumz.playing = false;
@@ -107,12 +88,77 @@ $(document).ready(function(){
 				}				
 			}
 		},
-		tracker: tracker, 
-		sounds: sounds
+		play: function () {
+			var beat = $.drumz.tracker.activate_next();
+			// Find each active beat, play it
+			var audio;
+			var column = $(".soundrow[id^=control] li.pip:nth-child("+(beat+1).toString()+")");
+
+			column.active().each(function (){
+				audio = document.getElementById($(this).data('sound_id'));
+				if (!audio.paused) {
+					// Pause and reset it
+					audio.pause();
+					audio.currentTime = 0.0;
+				}
+				audio.play();
+			});
+		},
+		serialize: function () {
+			return $(".soundrow[id^=control] li.pip").map(function () {
+				return $(this).active().length;
+			}).get().
+			concat('|', $.drumz.tempo).
+			join('');
+		},
+		deserialize: function (str) {
+			// Split it up, work it out, removing the actual hashmark
+			var pieces = str.split('|');
+			// Set the lights
+			var lights = pieces[0];
+			$(".soundrow[id^=control] li.pip").each(function(i){
+				// Make sure we haven't exceeded
+				if (i >= lights.length) 
+					return false;
+				if (lights.charAt(i) == '1')
+					$(this).addClass('active');
+			});
+			// Set the tempo
+			if (typeof pieces[1] !== 'undefined') {
+				$('#temposlider').slider('value', parseInt(pieces[1]));
+				$('#tempovalue').innerHTML = pieces[1];
+				$.drumz.tempo = parseInt(pieces[1]);
+			}
+			
+		},
+
+		// § Components that make up the drumz §
+		tracker: {
+			pips: $("#tracker .pip"),
+			// deactivates the current pip and activates the next pip
+			// starts at pip 0, wraps around to pip 0
+			// returns the index of the newly activated pip
+			activate_next: function () {
+				var pips = this.pips;
+				return pips.index(
+					$(pips.active().deactivate().next()[0] || pips[0]).activate()
+				);
+			},
+		}, 
+		sounds: {
+			
+		},
+		notes: {
+			clear: function () {
+				$(".soundrow[id^=control] li.active").deactivate();
+			}
+		}
 	}});
 	
+	// apply defaults
 	$.extend($.drumz, $.drumz.defaults);
 	
+	// add element helper methods
 	$.fn.extend({
 		active: function () {
 			return this.filter('.active');
@@ -126,60 +172,8 @@ $(document).ready(function(){
 	})
 })(jQuery);
 
-function playBeat() {	
-	var beat = $.drumz.tracker.activate_next();
-	// Find each active beat, play it
-	var audio;
-	var column = $(".soundrow[id^=control] li.pip:nth-child("+(beat+1).toString()+")");
-	
-	column.active().each(function (){
-		audio = document.getElementById($(this).data('sound_id'));
-		if (!audio.paused) {
-			// Pause and reset it
-			audio.pause();
-			audio.currentTime = 0.0;
-		}
-		audio.play();
-	});
-
-}
 
 // Make a new hash
 function buildHash() {
-	var newhash =
-	$(".soundrow[id^=control] li.pip").map(function () {
-		return $(this).active().length;
-	}).get().
-	concat('|', $.drumz.tempo).
-	join('');
-	location.hash = newhash;
+	location.hash = $.drumz.serialize();
 } 
-
-// Read in our hash
-function parseHash() {
-	if (location.hash.length > 0) {
-		// Split it up, work it out, removing the actual hashmark
-		var pieces = location.hash.substring(1).split('|');
-		// Set the lights
-		var lights = pieces[0];
-		$(".soundrow[id^=control] li.pip").each(function(i){
-			// Make sure we haven't exceeded
-			if (i >= lights.length) return false;
-			// Check our location, turn on class if need be
-			if (lights.charAt(i) == '1') {
-				$(this).addClass('active');
-			}
-		});
-		// Set the tempo
-		if (typeof pieces[1] !== 'undefined') {
-			$('#temposlider').slider('value', parseInt(pieces[1]));
-			$('#tempovalue').innerHTML = pieces[1];
-			$.drumz.tempo = parseInt(pieces[1]);
-		}
-	}
-}
-
-// Clear it!
-function clearAll() {
-	$(".soundrow[id^=control] li.active").deactivate();
-}
